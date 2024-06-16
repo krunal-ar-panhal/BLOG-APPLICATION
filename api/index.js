c5701ca2590eb4f32eff45f871f5bc3ee3dd3bc4
krunal-ar-panchal <155724372+krunal-ar-panchal@users.noreply.github.com>
2024-06-16 15:54:17 +0530

    complete blog application

---
+++ api/index.js
@@ -0,0 +1,49 @@
+import express from "express";
+import { connect } from "mongoose";
+import { config } from "dotenv";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import authRouter from "./Routes/authRoute.js";
+import userRouter from "./Routes/userRoute.js";
+import postRoutes from './Routes/postRoute.js'
+import commentRoutes from './Routes/commetRoute.js'
+import path from 'path';
+
+const app = express();
+config();
+
+const __dirname = path.resolve();
+
+const PORT = process.env.PORT;
+
+connect(process.env.MONGODB)
+  .then(() => {
+    console.log("MONGODB");
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors());
+
+app.use("/api/auth", authRouter);
+app.use("/api/user", userRouter);
+app.use('/api/post', postRoutes);
+app.use('/api/comment', commentRoutes);
+
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`${PORT}`);
+});
+
+app.get("/",(req,res)=>{
+  res.json('BLOG APP')
+})
+
